d4272699e43c58c00e8eae80e3b6103084ca4958
Ak6660 <ayushtyagi4971@gmail.com>
2023-10-09 20:38:06 +0530

    styleChanges

---
+++ src/routes/checkout/Checkout.jsx
@@ -0,0 +1,33 @@
+import { useCartContext } from "../../contexts/CartDropdownContext";
+import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+import { Button } from "../../components/button/Button";
+import { useNavigate } from "react-router-dom";
+
+const Checkout = () => {
+  const { cartItems } = useCartContext();
+  // const {name,price,quantity,id,imageUrl} = cartItems
+  const navigate = useNavigate();
+
+  const handleGotoShop = () => {
+    navigate("/shop");
+  };
+
+  if (!cartItems.length) {
+    return (
+      <div>
+        <h2>Empty Cart</h2>
+        <Button onClick={handleGotoShop}>Go to Shop</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} product={item} />
+      ))}
+    </div>
+  );
+};
+
+export default Checkout;
